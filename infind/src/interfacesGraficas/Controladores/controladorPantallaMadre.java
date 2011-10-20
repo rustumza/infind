6ddb9d5eea3bea2c5b22e3ca8274c7f1cb7ee579
guillepuntons <guillepuntons@gmail.com>
2011-10-20 00:26:58 +0000

    sistema

---
+++ infind/src/interfacesGraficas/Controladores/controladorPantallaMadre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesGraficas.Controladores;
+
+import interfacesGraficas.PantallaMadre;
+
+/**
+ *
+ * @author rustu
+ */
+public class controladorPantallaMadre {
+
+    PantallaMadre pantalla;
+    
+    public void iniciar() {
+        pantalla = new PantallaMadre();
+        pantalla.setVisible(true);
+    }
+    
+    
+    
+}
