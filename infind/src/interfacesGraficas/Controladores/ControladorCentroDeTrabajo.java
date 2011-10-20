8d454d11d5351484dfa088282f692a8ae105ed07
gringodiego2011 <gringodiego2011@gmail.com>
2011-10-20 20:38:27 +0000

    Arreglado lo de Guardar y Buscar Producto

---
+++ infind/src/interfacesGraficas/Controladores/ControladorCentroDeTrabajo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesGraficas.Controladores;
+
+import interfacesGraficas.PantallaCrearCentro;
+
+/**
+ *
+ * @author diego
+ */
+public class ControladorCentroDeTrabajo {
+    private PantallaCrearCentro pantallacrearcentro;
+
+    
+    public ControladorCentroDeTrabajo() {
+        pantallacrearcentro = new PantallaCrearCentro(null, false);
+        
+    }
+    
+}
