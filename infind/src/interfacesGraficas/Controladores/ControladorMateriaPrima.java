6f47ac0cd2a4f79f2393761a5e7a0412624134b5
guillepuntons <guillepuntons@gmail.com>
2011-10-26 15:20:47 +0000

    sistema

--- infind/src/interfacesGraficas/Controladores/ControladorMateriaPrima.java
+++ infind/src/interfacesGraficas/Controladores/ControladorMateriaPrima.java
@@ -4,7 +4,9 @@
  */
 package interfacesGraficas.Controladores;
 
+import Entidades.MateriaPrima;
 import interfacesGraficas.PantallaCrearMateriaPrima;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,14 +15,34 @@ import interfacesGraficas.PantallaCrearMateriaPrima;
 public class ControladorMateriaPrima {
 
     ControladorPantallaMadre controladorPantallaMadre;
+    PantallaCrearMateriaPrima pantallaCrearMateriPrima;
     
     ControladorMateriaPrima(ControladorPantallaMadre contPantMad) {
         controladorPantallaMadre = contPantMad;
     }
 
     public void crearMateriaPrima() {
-        PantallaCrearMateriaPrima pantalla = new PantallaCrearMateriaPrima(controladorPantallaMadre.getPantalla(), false);
-        pantalla.setVisible(true);
+        pantallaCrearMateriPrima = new PantallaCrearMateriaPrima(controladorPantallaMadre.getPantalla(), false, this);
+        pantallaCrearMateriPrima.setVisible(true);
+    }
+
+    public void guardar() {
+        
+        //TODO bloquear botones de gaurdar, cancear. etc
+        
+        MateriaPrima matPrim = new MateriaPrima();
+        matPrim.setCodigo(pantallaCrearMateriPrima.getCodigoTextBox().getText());
+        if(pantallaCrearMateriPrima.getNombreTextBox().getText().isEmpty() | pantallaCrearMateriPrima.getNombreTextBox().getText().equals(" ")){
+                JOptionPane.showMessageDialog(pantallaCrearMateriPrima, "Debe ingresar un nombre para la materia prima", "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
+                pantallaCrearMateriPrima.getNombreTextBox().requestFocus();
+                return;
+        }else{
+            matPrim.setNombre(pantallaCrearMateriPrima.getNombreTextBox().getText());
+        }
+        matPrim.setDescripcion(pantallaCrearMateriPrima.getDescripcionTextArea().getText());
+        matPrim.setUnidadDeMedida((String)pantallaCrearMateriPrima.getUnidadDeMedidaListBox().getModel().getSelectedItem());
+        matPrim.setCategoria(((String)pantallaCrearMateriPrima.getCategoriaListBox().getModel().getSelectedItem()).charAt(0));
+        
     }
     
 }
