c0716eeb975706dfbc22c82326793e4bdc7b9e48
gringodiego2011@gmail.com <gringodiego2011@gmail.com@41e9630a-7ef5-7e52-fbf0-3d3150767221>
2011-10-26 20:38:27 +0000

    nada

--- trunk/infind/src/infind/Main.java
+++ trunk/infind/src/infind/Main.java
@@ -2,17 +2,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package infind;
 
+import Entidades.MaestroDeArticulo;
+import Entidades.MaestroDeCentroDeTrabajo;
 import de.javasoft.plaf.synthetica.SyntheticaSimple2DLookAndFeel;
+import excepciones.ExpertoCentroDeTrabajoException;
 import interfacesGraficas.Controladores.ControladorPantallaMadre;
 import java.text.ParseException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 import persistencia.Conexion;
+import persistencia.Fachada;
 
 /**
  *
@@ -35,7 +41,19 @@ public class Main {
         Conexion.getInstancia().getSession();
         //ControladorPantallaMadre cpm = new ControladorPantallaMadre();
         //cpm.iniciar();
+
+        List<MaestroDeArticulo> articulosDisponibles = null;
+        Criteria criterioArticulo = Fachada.getInstancia().crearCriterio(MaestroDeArticulo.class);
+        criterioArticulo.add(Restrictions.eq("eliminado", true));
+        articulosDisponibles = Fachada.getInstancia().buscar(MaestroDeArticulo.class, criterioArticulo);
+        for (MaestroDeArticulo maestroDeArticulo : articulosDisponibles) {
+            System.out.println("Eliminado ??? : " + maestroDeArticulo.getEliminado());
+        }
+
+
+
+
+
         new ControladorPantallaMadre().iniciar();
     }
-
 }
