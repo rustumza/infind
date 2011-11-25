723281f7ae6c29e1458435152c4f4ab59ea8c201
guillepuntons <guillepuntons@gmail.com>
2011-11-25 21:17:36 +0000

    sistema

--- infind/src/interfacesGraficas/Controladores/ControladorCrearOrdenDeFabricacion.java
+++ infind/src/interfacesGraficas/Controladores/ControladorCrearOrdenDeFabricacion.java
@@ -4,10 +4,30 @@
  */
 package interfacesGraficas.Controladores;
 
+import interfacesGraficas.PantallaCrearOrdenDeFabricacion;
+
 /**
  *
  * @author rustu
  */
 public class ControladorCrearOrdenDeFabricacion {
+    
+    
+    ControladorPantallaMadre controladorPantallaMadre;
+    PantallaCrearOrdenDeFabricacion pantalla;
+    
+    
+    public ControladorCrearOrdenDeFabricacion(ControladorPantallaMadre aThis) {
+        controladorPantallaMadre = aThis;
+        
+    }
+    
+    public void iniciar(){
+    
+        pantalla = new PantallaCrearOrdenDeFabricacion(controladorPantallaMadre.getPantalla(), false, this);
+        pantalla.setLocationRelativeTo(null);
+        pantalla.setVisible(true);
+    }
+    
     
 }
